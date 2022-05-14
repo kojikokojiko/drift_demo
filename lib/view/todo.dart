94b75803c968e322ddaa2da3726b3cf3c67b7a0a
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-05-14 16:51:38 +0900

    ボタン修正

--- lib/view/todo.dart
+++ lib/view/todo.dart
@@ -1,6 +1,7 @@
 import 'package:drift_demo/model/db/todo_db.dart';
 import 'package:drift_demo/view/todo_vm.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
@@ -104,53 +105,48 @@ class Todo extends HookConsumerWidget  {
                             temp = temp.copyWith(description: value);
                           },
                         ),
-                        Table(
-                          defaultVerticalAlignment:
-                              TableCellVerticalAlignment.values[0],
-                          children: [
-                            TableRow(
-                              children: [
-                                ElevatedButton(
-                              
 
-
-                                  onPressed: () {
-                                    DatePicker.showDateTimePicker(
-                                      context,
-                                      showTitleActions: true,
-                                      minTime: DateTime.now(),
-                                      onConfirm: (date) {
-                                        limit.value = date;
-                                        print(limit);
-                                        temp = temp.copyWith(limit: date);
-                                      },
-                                      currentTime: DateTime.now(),
-                                      locale: LocaleType.jp,
-                                    );
+                        Row(
+                          mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                          children:[
+                            ElevatedButton(
+                              onPressed: () {
+                                DatePicker.showDateTimePicker(
+                                  context,
+                                  showTitleActions: true,
+                                  minTime: DateTime.now(),
+                                  onConfirm: (date) {
+                                    limit.value = date;
+                                    print(limit);
+                                    temp = temp.copyWith(limit: date);
                                   },
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.calendar_today),
-                                      Text(limit.value == null
-                                          ? ""
-                                          : limit.value.toString().substring(0, 10)),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  width: 10,
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    todoProvider.writeData(temp);
-                                    Navigator.pop(context3);
-                                  },
-                                  child: Text('OK'),
-                                ),
-                              ],
+                                  currentTime: DateTime.now(),
+                                  locale: LocaleType.jp,
+                                );
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(Icons.calendar_today),
+                                  Text(limit.value == null
+                                      ? ""
+                                      : limit.value.toString().substring(0, 10)),
+                                ],
+                              ),
                             ),
-                          ],
-                        ),
+                            Container(
+                              width: 10,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                todoProvider.writeData(temp);
+                                Navigator.pop(context3);
+                              },
+                              child: Text('OK'),
+                            ),
+
+                          ]
+                        )
+
                       ],
                     ),
                   );
